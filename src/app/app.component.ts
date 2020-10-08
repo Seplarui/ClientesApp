d429c4f20eb3350c69fc385b39d8c96c389f2fb7
Sebas <seplarui@gmail.com>
2020-10-08 21:31:18 +0200

    bootstrap/navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido al curso de Angular';
+  curso = 'Curso Angular';
+  profesor = 'Profesor Bacterio';
 }
